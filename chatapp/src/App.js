645f0cf83d85b7b23a29c698cee00ed4fada61c6
Josh Lawlor <joshlawlor1@gmail.com>
2023-10-05 17:12:00 -0700

    All basic pages added and routes functional

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
 
+
+//PAGE IMPORTS:
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path='/' element={<Landing/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/chat' element={<Chat/>}/>
+    </Routes>
   );
 }
 
