3ad3a3810840ce67978060a616ab248e0deaa1db
Josh Lawlor <joshlawlor1@gmail.com>
2023-10-07 19:29:17 -0700

    Chat room displays /chat with specific room info. Chat firestor query still in testing

--- chatapp/src/components/Chat.jsx
+++ chatapp/src/components/Chat.jsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Navbar from "./Navbar";
+import { useLocation } from "react-router-dom";
+import { auth, db } from "../firebase";
+import {
+  query,
+  doc,
+  collection,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [chatUser] = useAuthState(auth);
+  const location = useLocation();
+  const roomID = location.state.id;
+  const roomName = location.state.name;
+  const roomOwner = location.state.owner;
+  const userList = location.state.userList;
+
+  console.log(roomID);
+  const chatRoomRef = doc(collection(db, "chatRooms"), roomID);
+  //   //THIS GRABS THE SPECIFIC MESSAGES SUBCOLLECTION FROM THE ROOM DOC
+  const messagesSubcollectionRef = collection(chatRoomRef, "Messages");
+  const q = query(messagesSubcollectionRef, orderBy("timestamp"));
+  console.log(q);
+  // useEffect(() => {
+  //   console.log("USEFFECT RAN");
+  //   //THIS GRABS THE SPECIFIC ROOM DOC FROM THE CHATS COLLECTION
+  //   const chatRoomRef = doc(collection(db, "chatRooms"), roomID);
+  //   //THIS GRABS THE SPECIFIC MESSAGES SUBCOLLECTION FROM THE ROOM DOC
+  //   const messagesSubcollectionRef = collection(chatRoomRef, "Messages");
+
+  //   const q = query(messagesSubcollectionRef, orderBy("timestamp"));
+  //   console.log(q);
+  //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  //     let messages = [];
+  //     querySnapshot.forEach((doc) => {
+  //       messages.push({ ...doc.data(), id: doc.id });
+  //     });
+  //     setMessages(messages);
+  //   });
+
+  //   return () => unsubscribe();
+  // }, [roomName, roomOwner, roomID, chatUser]);
+
   return (
     <ChatWrapper>
-      <Navbar/>
-      <ContentWrapper></ContentWrapper>
+      <Navbar />
+      <ContentWrapper>
+        <Heading>Chat Room: {roomName}</Heading>
+      </ContentWrapper>
     </ChatWrapper>
   );
 };
@@ -20,3 +66,5 @@ const ContentWrapper = styled.div`
   flex: 1;
   margin-left: 15vw; /* Adjust the left margin to match the Navbar width */
 `;
+
+const Heading = styled.h1``;
--- chatapp/src/components/ChatElements/ChatRooms.jsx
+++ chatapp/src/components/ChatElements/ChatRooms.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import Room from "./RoomElement";
+// import Room from "./RoomElement";
 
 //FIREBASE IMPORTS:
-import { db, auth } from "../../firebase";
+import { db } from "../../firebase";
 import { query, collection, onSnapshot } from "firebase/firestore";
 
 const ChatRooms = () => {
+  const navigate = useNavigate();
   const [rooms, setRooms] = useState([]);
+
+  //FIRESTORE CHAT ROOMS QUERY:
   useEffect(() => {
     const q = query(collection(db, "chatRooms"));
     console.log(q);
@@ -20,12 +24,31 @@ const ChatRooms = () => {
     });
     return () => unsubscribe();
   }, []);
+
+  const openRoom = (room) => {
+    navigate("/chat", {
+      state: {
+        id: room.id,
+        name: room.name,
+        owner: room.owner,
+        userList: room.userList,
+      },
+    });
+  };
+
   return (
     <ChatWrapper>
       <ContentWrapper>
         {rooms &&
           rooms.map((room) => (
-            <Room key={room.id} room={room} owner={room.owner} />
+            <Room
+              onClick={() => openRoom(room)}
+              key={room.id}
+              room={room}
+              owner={room.owner}
+            >
+              <p className="name">{room.name}</p>
+            </Room>
           ))}
       </ContentWrapper>
     </ChatWrapper>
@@ -35,5 +58,17 @@ const ChatRooms = () => {
 export default ChatRooms;
 const ChatWrapper = styled.div``;
 
-const ContentWrapper = styled.div`
+const ContentWrapper = styled.div``;
+
+const Room = styled.div`
+  display: inline-block;
+  color: #444;
+  border: 1px solid #ccc;
+  background: #ddd;
+  box-shadow: 0 0 5px -1px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  vertical-align: middle;
+  max-width: 100px;
+  padding: 5px;
+  text-align: center;
 `;
