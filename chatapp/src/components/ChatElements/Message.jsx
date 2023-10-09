4d7565f4f4e03854f23ea736ec12908bb0cc1bc6
Josh Lawlor <joshlawlor1@gmail.com>
2023-10-09 11:47:31 -0700

    Added basic styling to message element. If received or sent, color should change

--- chatapp/src/components/ChatElements/Message.jsx
+++ chatapp/src/components/ChatElements/Message.jsx
@@ -1,106 +1,60 @@
-import React from 'react'
-import { auth } from "../../firebase";
-const Message = ({message}) => {
-  const messageAuthor = message.uid
-  const messageClass = 
-  messageAuthor === auth.currentUser.uid 
-  ? `${'message-sent'}`
-  : `${'message-received'}`
+import React from 'react';
+import { auth } from '../../firebase';
+import styled, { css } from 'styled-components';
+
+const Message = ({ message }) => {
+  const messageAuthor = message.uid;
+
+  const messageStyles = css`
+    position: relative;
+    margin-bottom: 10px;
+    padding: 10px;
+    max-width: 50%;
+    width: fit-content;
+    text-align: left;
+    font: 400 0.9em 'Open Sans', sans-serif;
+    border-radius: 10px;
+    align-self: ${messageAuthor === auth.currentUser.uid ? 'flex-end' : 'flex-start'};
+  `;
+
+  const MessageContainer = styled.div`
+    ${messageStyles}
+    background-color: ${messageAuthor === auth.currentUser.uid ? '#f8e896' : '#a8ddfd'};
+    border: 1px solid ${messageAuthor === auth.currentUser.uid ? '#f8e896' : '#97c6e3'};
+
+    &:after {
+      content: '';
+      position: absolute;
+      width: 0;
+      height: 0;
+      top: 0;
+      left: ${messageAuthor === auth.currentUser.uid ? 'auto' : '-15px'};
+      right: ${messageAuthor === auth.currentUser.uid ? '-15px' : 'auto'};
+      border-top: 15px solid ${messageAuthor === auth.currentUser.uid ? '#f8e896' : '#a8ddfd'};
+      border-left: ${messageAuthor === auth.currentUser.uid ? '15px solid transparent' : 'transparent'};
+      border-right: ${messageAuthor === auth.currentUser.uid ? 'transparent' : '15px solid transparent'};
+    }
+
+    &:before {
+      content: '';
+      position: absolute;
+      width: 0;
+      height: 0;
+      top: -1px;
+      left: ${messageAuthor === auth.currentUser.uid ? 'auto' : '-17px'};
+      right: ${messageAuthor === auth.currentUser.uid ? '-17px' : 'auto'};
+      border-top: 17px solid ${messageAuthor === auth.currentUser.uid ? '#dfd087' : '#97c6e3'};
+      border-left: ${messageAuthor === auth.currentUser.uid ? '16px solid transparent' : 'transparent'};
+      border-right: ${messageAuthor === auth.currentUser.uid ? 'transparent' : '16px solid transparent'};
+    }
+  `;
+
   return (
-  
-        <div className={`${messageClass}`}>
-            <p class="message-content">{message.name}</p>
-            <p>{message.text}</p>
-        </div>
+    <MessageContainer>
+      <p className="message-content">{message.name}</p>
+      <p>{message.text}</p>
+    </MessageContainer>
+  );
+};
 
-    
-  )
-}
-
-export default Message
-// .message-received {
-//     position: relative;
-//     margin-right: 20px;
-//     margin-left: 20px;
-//     margin-bottom: 10px;
-//     padding: 10px;
-//     max-width: 50%;
-//     background-color: #a8ddfd;
-//     width: fit-content;
-//     text-align: left;
-//     font: 400 0.9em "Open Sans", sans-serif;
-//     border: 1px solid #97c6e3;
-//     border-radius: 10px;
-//     align-self: flex-start;
-//   }
-//   .message-content {
-//     padding: 0;
-//     margin: 0;
-//   }
-  
-//   .message-received:after {
-//     content: "";
-//     position: absolute;
-//     width: 0;
-//     height: 0;
-//     border-top: 15px solid #a8ddfd;
-//     border-left: 15px solid transparent;
-//     border-right: 15px solid transparent;
-//     top: 0;
-//     left: -15px;
-//   }
-  
-//   .message-received:before {
-//     content: "";
-//     position: absolute;
-//     width: 0;
-//     height: 0;
-//     border-top: 17px solid #97c6e3;
-//     border-left: 16px solid transparent;
-//     border-right: 16px solid transparent;
-//     top: -1px;
-//     left: -17px;
-//   }
-  
-//   .message-sent {
-//     position: relative;
-//     margin-left: 20px;
-//     margin-right: 20px;
-//     margin-bottom: 10px;
-//     padding: 10px;
-//     max-width: 50%;
-//     background-color: #f8e896;
-//     width: fit-content;
-//     text-align: left;
-//     font: 400 0.9em "Open Sans", sans-serif;
-//     border: 1px solid #f8e896;
-//     border-radius: 10px;
-//     background-color: #f8e896;
-//     align-self: flex-end;
-//   }
-  
-//   .message-sent:after {
-//       content: '';
-//       position: absolute;
-//       width: 0;
-//       height: 0;
-//       border-bottom: 15px solid #f8e896;
-//       border-left: 15px solid transparent;
-//       border-right: 15px solid transparent;
-//       bottom: 0;
-//       right: -15px;
-    
-//   }
-  
-//   .message-sent:before {
-//       content: '';
-//       position: absolute;
-//       width: 0;
-//       height: 0;
-//       border-bottom: 17px solid #dfd087;
-//       border-left: 16px solid transparent;
-//       border-right: 16px solid transparent;
-//       bottom: -1px;
-//       right: -17px;
-//   }
-  
+export default Message;
