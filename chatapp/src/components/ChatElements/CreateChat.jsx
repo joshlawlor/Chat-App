ce6184eecf1a06c1680c37ca59fcba82c9a737c0
Josh Lawlor <joshlawlor1@gmail.com>
2023-10-10 21:50:24 -0700

    Algolia search functional. Username selected is added to chat userList.

--- chatapp/src/components/ChatElements/CreateChat.jsx
+++ chatapp/src/components/ChatElements/CreateChat.jsx
@@ -11,8 +11,8 @@ import {
 } from "firebase/firestore";
 
 //ALGOLIA REFERENCES::
-const algoliaAppID = process.env.ALGOLIA_APP_ID;
-const algoliaApiKey = process.env.ALGOLIA_API_KEY;
+const algoliaAppID = process.env.REACT_APP_ALGOLIA_APP_ID;
+const algoliaApiKey = process.env.REACT_APP_ALGOLIA_API_KEY;
 const algoliasearch = require("algoliasearch");
 const client = algoliasearch(algoliaAppID, algoliaApiKey);
 const index = client.initIndex("chat-users");
@@ -23,13 +23,55 @@ const CreateChat = () => {
 
   //SEARCH STATES:
   const [userList, setUserList] = useState([]);
-
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null);
   //FIRESTORE REFERENCES:
   const chatCollectionRef = collection(db, "chatRooms");
   const chatDocRef = doc(chatCollectionRef);
   const messagesSubcollectionRef = collection(chatDocRef, "Messages");
 
-  //SEARCH USERS FUNCTION:
+  //SEARCH USERS FUNCTIONS:
+
+  const handleSearchInputChange = (e) => {
+    //**NEED TO PUT A CHECK HERE IN CASE OF BLANK SEARCH */
+    const value = e.target.value;
+    setSearchInput(value);
+    if (searchTimeout) {
+      clearTimeout(searchTimeout);
+    }
+    setSearchTimeout(
+      setTimeout(() => {
+        searchUsers(value);
+      }, 500)
+    );
+  };
+  const searchUsers = (value) => {
+    if (value === "" || value === " ") {
+      alert("Please enter a valid chat username");
+      return;
+    }
+    setSearchInput("");
+    index
+      .search(value)
+      .then(({ hits }) => {
+        console.log("USER SEARCH", hits);
+        setSearchResults(hits);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleUserChange = (index, value) => {
+    const updatedUserList = [...userList];
+    if (!updatedUserList.includes(value)) {
+      // If it doesn't exist, add it to the end of the array
+      updatedUserList.push(value);
+    }
+    setUserList([...updatedUserList]);
+    console.log(userList);
+  };
 
   //CREATE CHAT FUNCTION:
   const createChat = async (e) => {
@@ -77,6 +119,23 @@ const CreateChat = () => {
             type="text"
             placeholder="Chat Name"
           />
+          <input
+            value={searchInput}
+            onChange={(e) => handleSearchInputChange(e)}
+            type="text"
+            placeholder="Search for user (username)"
+          />
+          <div>
+            {searchResults.map((result, index) => (
+              <button
+                type="button"
+                key={result.objectID}
+                onClick={() => handleUserChange(index, result.username)}
+              >
+                {result.username}
+              </button>
+            ))}
+          </div>
           <button type="button" onClick={() => cancelChatCreation()}>
             Cancel
           </button>
@@ -93,4 +152,7 @@ const CreateChatWrapper = styled.div``;
 
 const ContentWrapper = styled.div``;
 
-const CreateForm = styled.form``;
+const CreateForm = styled.form`
+display: flex
+flex-direction: column;
+`;
